abb9517d6897fa657aedaeb12470f0a01993cbfa
luiscal4a <luiscal4a@gmail.com>
2019-10-05 21:20:19 +0200

    Deleted map

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { interopDefault } from './utils'
 
-const _fd3556ee = () => interopDefault(import('..\\pages\\bookManagement.vue' /* webpackChunkName: "pages_bookManagement" */))
-const _47b038da = () => interopDefault(import('..\\pages\\createOffer.vue' /* webpackChunkName: "pages_createOffer" */))
-const _5a3cd008 = () => interopDefault(import('..\\pages\\login.vue' /* webpackChunkName: "pages_login" */))
-const _ddea952c = () => interopDefault(import('..\\pages\\offerCatalogue.vue' /* webpackChunkName: "pages_offerCatalogue" */))
-const _38e6af60 = () => interopDefault(import('..\\pages\\register.vue' /* webpackChunkName: "pages_register" */))
-const _6c78fdb6 = () => interopDefault(import('..\\pages\\search.vue' /* webpackChunkName: "pages_search" */))
-const _34c15604 = () => interopDefault(import('..\\pages\\upload.vue' /* webpackChunkName: "pages_upload" */))
-const _0939f4e5 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
+const _25eb7a19 = () => interopDefault(import('..\\pages\\bookManagement.vue' /* webpackChunkName: "pages_bookManagement" */))
+const _5efe2803 = () => interopDefault(import('..\\pages\\createOffer.vue' /* webpackChunkName: "pages_createOffer" */))
+const _99c01f28 = () => interopDefault(import('..\\pages\\login.vue' /* webpackChunkName: "pages_login" */))
+const _3590dafa = () => interopDefault(import('..\\pages\\offerCatalogue.vue' /* webpackChunkName: "pages_offerCatalogue" */))
+const _9fc77e20 = () => interopDefault(import('..\\pages\\register.vue' /* webpackChunkName: "pages_register" */))
+const _1d5f9296 = () => interopDefault(import('..\\pages\\search.vue' /* webpackChunkName: "pages_search" */))
+const _0d2c0a8e = () => interopDefault(import('..\\pages\\upload.vue' /* webpackChunkName: "pages_upload" */))
+const _2d0f6556 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
 Vue.use(Router)
 
@@ -87,35 +87,35 @@ export function createRouter() {
 
     routes: [{
       path: "/bookManagement",
-      component: _fd3556ee,
+      component: _25eb7a19,
       name: "bookManagement"
     }, {
       path: "/createOffer",
-      component: _47b038da,
+      component: _5efe2803,
       name: "createOffer"
     }, {
       path: "/login",
-      component: _5a3cd008,
+      component: _99c01f28,
       name: "login"
     }, {
       path: "/offerCatalogue",
-      component: _ddea952c,
+      component: _3590dafa,
       name: "offerCatalogue"
     }, {
       path: "/register",
-      component: _38e6af60,
+      component: _9fc77e20,
       name: "register"
     }, {
       path: "/search",
-      component: _6c78fdb6,
+      component: _1d5f9296,
       name: "search"
     }, {
       path: "/upload",
-      component: _34c15604,
+      component: _0d2c0a8e,
       name: "upload"
     }, {
       path: "/",
-      component: _0939f4e5,
+      component: _2d0f6556,
       name: "index"
     }],
 
--- pages/index.vue
+++ pages/index.vue
@@ -98,33 +98,9 @@
         </b-card-group>
       </div>
     </div>
-
-    <div id="app">
-      <l-map :zoom="zoom" :center="center">
-        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-        <l-marker :lat-lng="marker"></l-marker>
-      </l-map>
-    </div>
   </section>
 </template>
 
-<script>
-  var { LMap, LTileLayer, LMarker } = Vue2Leaflet;
-
-  new Vue({
-    el: '#app',
-    components: { LMap, LTileLayer, LMarker },
-    data() {
-      return {
-        zoom:13,
-        center: L.latLng(47.413220, -1.219482),
-        url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-        attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        marker: L.latLng(47.413220, -1.219482),
-      }
-    }
-  });
-</script>
 
 <script>
 export default {
@@ -134,7 +110,7 @@ export default {
     changePage() {
       console.log("Hey");
       this.$router.push({
-        name: "patata",
+        name: "login",
         params: { name: "world" }
       });
     }
